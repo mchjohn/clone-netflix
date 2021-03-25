1297547765cbe4a2f1ebf489ad5385856f7ae24a
Michel John <michel.john@hotmail.com>
2021-03-25 13:43:55 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import './App.css'
+
+import tmdb from './tmdb'
+import MovieRow from './components/MovieRow'
+import FeatureMovie from './components/FeatureMovie'
+import Header from './components/Header'
+
+export default () => {
+  const [movieList, setMovieList] = useState([])
+  const [featureData, setFeatureData] = useState(null)
+  const [blackHeader, setBlackHeader] = useState(false)
+
+  useEffect(() => {
+    const loadAll = async () => {
+      // Pegando a lista toda
+      let list = await tmdb.getHomeList()
+
+      setMovieList(list)
+
+      // Pegar feature
+      let originals = list.filter(i => i.slug === 'originals')
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1))
+      let chosen = originals[0].items.results[randomChosen]
+      let chosenInfo =await tmdb.getMovieInfo(chosen.id, 'tv')
+      
+      setFeatureData(chosenInfo)
+    }
+
+    loadAll()
+  }, [])
+
+  useEffect(() => {
+    const scrollListener = () => {
+      if(window.scrollY > 10) {
+        setBlackHeader(true)
+      } else {
+        setBlackHeader(false)
+      }
+    }
+
+    window.addEventListener('scroll', scrollListener)
+
+    return () => {
+      window.removeEventListener('scroll', scrollListener)
+    }
+  }, [])
+
+  return  (
+    <div className='page'>
+      <Header black={blackHeader} />
+
+      {featureData &&
+        <FeatureMovie item={featureData} />
+      }
+      
+      <section className='lists'>
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+      
+      <footer>
+        Feito com <span role='img' aria-label='coração'>❤</span> por <a href='https://github.com/mchjohn'>Michel John</a><br />
+        Direitos de imagem Netflix<br/>
+        Dados api <a href='https://www.themoviedb.org/'>Themoviedb.org</a>
+      </footer>
+
+      {movieList.length <= 0 &&
+        <div className='loading'>
+          <img src='https://media.wired.com/photos/592744d3f3e2356fd800bf00/master/w_2560%2Cc_limit/Netflix_LoadTime.gif' alt='carregando' />
+        </div>
+      }
     </div>
-  );
+  )
 }
-
-export default App;
